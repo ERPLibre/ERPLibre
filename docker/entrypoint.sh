940fad8f050c43511b944e057515eebe382e8164
Michael Faille <michael@faille.io>
2020-05-31 09:29:29 -0400

    uber commit - see descriptions :
    - use "docker" folder directly (without sub folders)
    - use suffix to differentiate all docker files
    - create a Dockerfile for dev called Dockerfile.dev
    - refactor the base docker file to accept Dockerfile.dev changes. Change his name for Dockerfile.core.
    - Adapt the production docker file to accept Dockerfile.core and change his name for name Dockerfile.main

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,18 +3,39 @@
 set -e
 if [[ "$ENV" == "dev" ]] &&  [ ! -z ${CURRENT_UID} ]
 then
-    export HOME=/ERPLibre
+    export HOME=$ODOO_PREFIX
     cd $HOME
 
-
+    # As it's only possible to fetch git repos manifest from an git url, we create one using git-daemon.
     git daemon --base-path=. --export-all --reuseaddr --informative-errors --verbose &
     GIT_PID=$!
     echo "my repo"  $(git rev-parse --abbrev-ref HEAD)
-    repo init -u  git://127.0.0.1:9418/  -b $(git rev-parse --abbrev-ref HEAD)
+    repo init -u  git://127.0.0.1:9418/  -b $(git rev-parse --abbrev-ref HEAD) -m default.dev.xml
 
     repo sync
 
+    # After sync, terminate the git checkout. We don't need graceful kill as we don't do commit on the git repo during the operation.
     kill -9 $GIT_PID
+
+    ls $HOME/odoo/odoo-bin
+    # $? give the posix return value of the last command. 0 == success
+    # then IS_ODOO_FILE_EXIST : 0 == YES
+    IS_ODOO_FILE_EXIST=$?
+
+    if [ "$IS_ODOO_FILE_EXIST" -ne "0" ]; then
+        echo "The file $HOME/odoo/odoo-bin doesnt exist. Verify entrypoint.sh";
+        exit 1;
+    fi
+
+    # Add the odoo bins code on $PATH
+    echo PATH=$HOME/odoo/:$PATH
+
+    # Configure an alias to use "odoo-bin" as "odoo".
+    alias odoo=odoo-bin
+
+    repo_manifest_gen_org_prefix_path.py $ODOO_PREFIX/addons $ODOO_RC $ODOO_PREFIX/odoo/odoo.conf && head $ODOO_PREFIX/odoo/odoo.conf
+    export ODOO_RC=$ODOO_PREFIX/odoo/odoo.conf
+
 elif [[ "$ENV" == "dev" ]] && [  -z ${CURRENT_UID} ]
 then
     echo 'Please run as follows : CURRENT_UID=$(id -u):$(id -g) docker-compose up'
@@ -47,15 +68,15 @@ case "$1" in
     -- | odoo)
         shift
         if [[ "$1" == "scaffold" ]] ; then
-            exec odoo "$@"
+            exec odoo  "$@" || exec odoo-bin "$@"
         else
             wait-for-psql.py ${DB_ARGS[@]} --timeout=30
-            exec odoo "$@" "${DB_ARGS[@]}"
+            exec $ODOO_EXEC_BIN "$@" "${DB_ARGS[@]}"
         fi
         ;;
     -*)
         wait-for-psql.py ${DB_ARGS[@]} --timeout=30
-        exec odoo "$@" "${DB_ARGS[@]}"
+        exec $ODOO_EXEC_BIN "$@" "${DB_ARGS[@]}"
         ;;
     *)
         exec "$@"
