9fb31922d9984e64ffc2301878198b18e5066a07
Michael Faille <michael@faille.io>
2020-05-31 09:29:29 -0400

    uber commit - see descriptions :
    - use "docker" folder directly (without sub folders)
    - use suffix to differentiate all docker files
    - create a Dockerfile for dev called Dockerfile.dev
    - refactor the base docker file to accept Dockerfile.dev changes. Change his name for Dockerfile.core.
    - Adapt the production docker file to accept Dockerfile.core and change his name for name Dockerfile.main

---
+++ docker/wait-for-psql.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import argparse
+import psycopg2
+import sys
+import time
+
+
+if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('--db_host', required=True)
+    arg_parser.add_argument('--db_port', required=True)
+    arg_parser.add_argument('--db_user', required=True)
+    arg_parser.add_argument('--db_password', required=True)
+    # arg_parser.add_argument('--db_name', required=True)
+    arg_parser.add_argument('--timeout', type=int, default=10)
+
+    args = arg_parser.parse_args()
+
+    start_time = time.time()
+    print("Try connection to postgres...")
+
+    connected = False
+    error = ''
+    while ((time.time() - start_time) < args.timeout ) or connected is True:
+        try:
+
+            conn = psycopg2.connect(user=args.db_user, host=args.db_host, port=args.db_port, password=args.db_password, dbname="odoo")
+
+            break
+        except psycopg2.OperationalError as e:
+            error = e
+        else:
+            connected = True
+            conn.close()
+        print(".")
+        time.sleep(1)
+
+    if error:
+        print("Database connection failure: %s" % error, file=sys.stderr)
+        sys.exit(1)
