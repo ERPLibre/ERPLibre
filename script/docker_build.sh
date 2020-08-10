1a5f611bfd23f530fc65eed2106bdb45a840fc07
Mathieu Benoit <mathben@technolibre.ca>
2020-08-10 15:52:51 -0400

    [FIX] docker missing volume for config of odoo - fix of master password

--- script/docker_build.sh
+++ script/docker_build.sh
@@ -4,10 +4,10 @@ cd docker
 ARGS=--build-arg=WORKING_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 set -e
 # Build base
-docker build ${ARGS} -f Dockerfile.base -t technolibre/erplibre-base:12.0 .
+docker build ${ARGS} -f Dockerfile.base -t technolibre/erplibre-base:1.0.1 .
 
 # Build prod
-docker build ${ARGS} -f Dockerfile.prod.pkg -t technolibre/erplibre:12.0-pkg .
+docker build ${ARGS} -f Dockerfile.prod.pkg -t technolibre/erplibre:1.0.1 .
 
 cd ..
 docker-compose up -d
