bbaccb9cb24212b8c31bc8ccb538490755d9e39a
Mathieu Benoit <mathben@technolibre.ca>
2020-08-10 15:52:51 -0400

    WIP

--- script/build_docker.sh
+++ script/build_docker.sh
@@ -4,10 +4,10 @@ cd docker
 ARGS=--build-arg=WORKING_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 set -e
 # Build base
-docker build ${ARGS} -f Dockerfile.base -t technolibre/erplibre-base:12.0 .
+docker build ${ARGS} -f Dockerfile.base -t mathben/erplibre-base:1.0.2 .
 
 # Build prod
-docker build ${ARGS} -f Dockerfile.prod.pkg -t technolibre/erplibre:12.0-pkg .
+docker build ${ARGS} -f Dockerfile.prod.pkg -t mathben/erplibre:1.0.2 .
 
 cd ..
 docker-compose up -d
