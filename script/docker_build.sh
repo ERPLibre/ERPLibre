b5302319882632d149e003071d5aafb3dcd567f4
Mathieu Benoit <mathben@technolibre.ca>
2020-09-11 00:15:49 -0400

    [IMP] generate docker version from env_var.sh

--- script/docker_build.sh
+++ script/docker_build.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
+. ./env_var.sh
+
 cd docker
 
 ARGS=--build-arg=WORKING_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 set -e
 # Build base
-docker build ${ARGS} -f Dockerfile.base -t technolibre/erplibre-base:1.0.1 .
+docker build ${ARGS} -f Dockerfile.base -t ${ERPLIBRE_DOCKER_BASE_VERSION} .
 
 # Build prod
-docker build ${ARGS} -f Dockerfile.prod.pkg -t technolibre/erplibre:1.0.1 .
+docker build ${ARGS} -f Dockerfile.prod.pkg -t ${ERPLIBRE_DOCKER_PROD_VERSION} .
 
-cd ..
+cd -
+# Rewrite docker-compose
+./script/docker_update_version.py --version=${ERPLIBRE_VERSION} --base=${ERPLIBRE_DOCKER_BASE} --prod=${ERPLIBRE_DOCKER_PROD}
 docker-compose up -d
