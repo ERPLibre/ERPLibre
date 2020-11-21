0615f573d459383a4849ec2081f6165ffa2e1f6d
Mathieu Benoit <mathben@technolibre.ca>
2020-11-21 00:10:55 -0500

    [ADD] docker_build with release - when docker build without release, add hash of commit in version

--- script/docker_build.sh
+++ script/docker_build.sh
@@ -2,14 +2,31 @@
 . ./env_var.sh
 
 ARGS=""
+IS_RELEASE=false
+ERPLIBRE_DOCKER_BASE="technolibre/erplibre-base"
+ERPLIBRE_DOCKER_PROD="technolibre/erplibre"
 
 for arg in "$@"
 do
     if [ "$arg" == "--no-cache" ]
     then
         ARGS="${ARGS} --no-cache"
+    elif [ "$arg" == "--release" ]
+    then
+        IS_RELEASE=true
     fi
 done
+
+if [ "$IS_RELEASE" == false ]
+then
+  # Add commit hash when not a release
+  ERPLIBRE_VERSION="${ERPLIBRE_VERSION}_$(git rev-parse --short HEAD)"
+fi
+
+ERPLIBRE_DOCKER_BASE_VERSION="${ERPLIBRE_DOCKER_BASE}:${ERPLIBRE_VERSION}"
+ERPLIBRE_DOCKER_PROD_VERSION="${ERPLIBRE_DOCKER_PROD}:${ERPLIBRE_VERSION}"
+
+echo "Create docker ${ERPLIBRE_DOCKER_PROD_VERSION}"
 
 # Rewrite docker-compose
 ./script/docker_update_version.py --version=${ERPLIBRE_VERSION} --base=${ERPLIBRE_DOCKER_BASE} --prod=${ERPLIBRE_DOCKER_PROD}
