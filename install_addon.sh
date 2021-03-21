665cf66c7c6e548ecb22b2b70b23831a3bf256a9
Mathieu Benoit <mathben@technolibre.ca>
2021-03-21 15:19:54 -0400

    [FIX] repo_revert_git_diff...

--- install_addon.sh
+++ install_addon.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 ./run.sh --no-http --stop-after-init -d $1 -i $2 -u $2
+echo ""
--- script/repo_revert_git_diff_date_from_code_generator.py
+++ script/repo_revert_git_diff_date_from_code_generator.py
@@ -10,8 +10,6 @@ import re
 new_path = os.path.normpath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(new_path)
 
-from script.git_tool import GitTool
-
 _logger = logging.getLogger(__name__)
 
 
@@ -35,7 +33,8 @@ def get_config():
 
 def main():
     config = get_config()
-    rex = r"\s+(?=\d{2}(?:\d{2})?-\d{1,2}-\d{1,2}\b)"
+    # rex = r"\s+(?=\d{2}(?:\d{2})?-\d{1,2}-\d{1,2}\b)"
+    rex = r"[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0-5][0-9]"
     path = "./addons/TechnoLibre_odoo-code-generator-template"
     repo = git.Repo(path)
     supported_ext = [".xml", ".pot", ".po"]
@@ -44,18 +43,29 @@ def main():
         file_path = diff.a_path
         filename, file_extension = os.path.splitext(file_path)
         if file_extension in supported_ext:
+            file_real_path = os.path.join(path, diff.a_path)
+            if not os.path.isfile(file_real_path):
+                continue
             str_diff = repo.git.diff(diff.a_path)
             patch = PatchSet(str_diff)
             for modified_file in patch.modified_files:
                 lst_to_write = []
-                file_real_path = os.path.join(path, diff.a_path)
                 for hunk in modified_file:
                     nb_line_source = len(hunk.source)
                     nb_line_target = len(hunk.target)
-                    assert nb_line_source == nb_line_target, f"Not the same line of diff:\n{str_diff}"
-                    for i in range(nb_line_target):
+                    if nb_line_source != nb_line_target:
+                        # TODO support different line
+                        continue
+                    # try:
+                    #     assert nb_line_source == nb_line_target, f"Not the same line of diff:\n{str_diff}"
+                    # except Exception as e:
+                    #     print(e)
+                    for i in range(nb_line_source):
                         result_source = re.split(rex, hunk.source[i])
-                        result_target = re.split(rex, hunk.target[i])
+                        if i > len(hunk.target):
+                            result_target = re.split(rex, hunk.target[i])
+                        else:
+                            result_target = ""
                         if len(result_source) > 1 or len(result_target) > 1:
                             line_to_change = hunk.target_start + i - 1
                             lst_to_write.append((line_to_change, hunk.source[i][1:]))
