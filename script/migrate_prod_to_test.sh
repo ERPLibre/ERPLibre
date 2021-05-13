a3cc183e25708894a29cc8fb2c7479f04ae3ccda
Mathieu Benoit <mathben@technolibre.ca>
2021-05-13 01:03:09 -0400

    [ADD] migrate prod to test database

---
+++ script/migrate_prod_to_test.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+if [ $# -lt 1 ]; then
+    # TODO: print usage
+    echo "Missing database name"
+    exit 1
+fi
+source ./.venv/bin/activate
+python3 ./odoo/odoo-bin -c ./config.conf --limit-time-real 99999 --limit-time-cpu 99999 --stop-after-init -i user_test,disable_mail_server,disable_auto_backup --dev all -d $@
