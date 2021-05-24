0aadefd26e917f2cdf276a0f61bd667324bc12f5
Mathieu Benoit <mathben@technolibre.ca>
2021-05-23 21:10:00 -0400

    black: reduce max length to 79 char and experimental-string-processing

--- script/maintenance/black.sh
+++ script/maintenance/black.sh
@@ -2,4 +2,4 @@
 # This will format all python file
 # argument 1: directory or file to format
 source ./.venv/bin/activate
-black -l 100 -t py37 $@
+black -l 79 --experimental-string-processing -t py37 $@
