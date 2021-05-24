6dbf346c4bd06272d81df8e0258b73ba3841b02d
Alexandre Ferreira Benevides <afbenevides@polymtl.ca>
2021-05-24 04:54:24 +0100

    [FIX] verify wich os for raspian during install

--- script/install_dev.sh
+++ script/install_dev.sh
@@ -5,17 +5,18 @@
 ################################################################################
 
 if [[ "${OSTYPE}" == "linux-gnu" ]]; then
+     echo  "\n---- linux-gnu detected----"
     OS=$(lsb_release -si)
     if [[ "${OS}" == "Ubuntu" ]]; then
         echo  "\n---- linux-gnu installation process started ----"
         ./script/install_debian_dependency.sh
-    elif [[ "${OS}" == "Raspian" ]]; then
-        echo  "\n---- Raspberry Pi ARM installation process started ----"
-        ./script/install_raspian_dependency.sh
     else
         echo "Your Linux system is not supported."
     fi
 elif [[ "${OSTYPE}" == "darwin"* ]]; then
     echo  "\n---- Darwin installation process started ----"
     ./script/install_OSX_dependency.sh
+elif [[ "${OSTYPE}" == "linux-gnueabihf"* ]]; then
+    echo  "\n---- Raspberry Pi ARM installation process started ----"
+    ./script/install_raspian_dependency.sh
 fi
