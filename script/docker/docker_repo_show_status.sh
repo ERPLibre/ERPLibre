11e07fddf3f5ad2a1ab746f1a55f936dfe40d347
Mathieu Benoit <mathben@technolibre.ca>
2021-07-01 03:19:29 -0400

    [UPD] add return line in bash script

--- script/docker/docker_repo_show_status.sh
+++ script/docker/docker_repo_show_status.sh
@@ -6,5 +6,6 @@ BASENAME=$(basename "${CURRENT}")
 docker exec -u root -ti ${BASENAME}_ERPLibre_1 /bin/bash -c "\
 cd /ERPLibre; \
 ./.venv/repo forall -pc 'git status -s'; \
+echo ''; \
 git status -s; \
 "
--- script/repo_diff_stat_last_version.sh
+++ script/repo_diff_stat_last_version.sh
@@ -3,5 +3,7 @@
 LAST_TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
 ./.venv/repo forall -pc "git diff --stat ERPLibre/${LAST_TAG}..HEAD"
 
+echo ""
+
 # For actual repo
 git diff --stat ${LAST_TAG}..HEAD
