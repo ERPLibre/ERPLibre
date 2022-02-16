2d15e8b8f6ca65c8d94887cf0efaa9ccc8941ccb
Alexandre Ferreira Benevides <alexandre.ferreira.benevides@gmail.com>
2022-02-15 20:12:10 -0500

    [FIX] clean script activation
    Signed-off-by: Alexandre Ferreira Benevides <alexandre.ferreira.benevides@gmail.com>

--- script/clean_system_for_install.sh
+++ script/clean_system_for_install.sh
@@ -10,9 +10,10 @@ read answer
  if [ $answer == "y" ]
  then
    echo "Ok we destroy"
-    # ./delete_production.sh
-    # sudo rm -rf ~/.poetry
-     #sudo rm -rf ~/.pyenv
+     ./script/delete_production.sh
+     sudo rm -rf ~/.poetry
+     sudo rm -rf ~/.pyenv
  else
      echo "Ok we cancel Destroy sequence"
  fi
+
