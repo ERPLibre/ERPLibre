aac73b57dde797ce71048cd84e4b5b737687c508
Mathieu Benoit <mathben@technolibre.ca>
2022-05-27 01:58:50 -0400

    [FIX] script prettier_xml: force print_width to maximum - Some XML string broke when formating, adding space in url by example

--- script/maintenance/prettier_xml.sh
+++ script/maintenance/prettier_xml.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 # This will format all xml
 # argument 1: directory or file to format
-prettier --xml-whitespace-sensitivity "ignore" --prose-wrap always --tab-width 4 --no-bracket-spacing --print-width 120 --write $@
+STR_ARG="'$*'"
+if [[ "${STR_ARG}" = *"/data/"* ]]; then
+  # Need to keep width with data, else add corrupted string data
+  prettier --xml-whitespace-sensitivity "ignore" --prose-wrap always --tab-width 4 --no-bracket-spacing --print-width 999999999 --write $@
+else
+  prettier --xml-whitespace-sensitivity "ignore" --prose-wrap always --tab-width 4 --no-bracket-spacing --print-width 120 --write $@
+fi
 retVal=$?
 if [[ $retVal -ne 0 ]]; then
     echo "Error prettier format"
