24bb559ed9441810eea80d4103b00c39fc156c35
Mathieu Benoit <mathben@technolibre.ca>
2022-08-29 11:16:51 -0400

    [FIX] script docker: docker-compose use lowercase name

--- script/docker/docker_exec.sh
+++ script/docker/docker_exec.sh
@@ -4,5 +4,7 @@ CURRENT=$(pwd)
 BASENAME=$(basename "${CURRENT}")
 # Docker remove dot
 BASENAME="${BASENAME//./}"
+# Lowercase
+BASENAME="${BASENAME,,}"
 
 docker exec -u root -ti ${BASENAME}_ERPLibre_1 bash
--- script/docker/docker_repo_show_status.sh
+++ script/docker/docker_repo_show_status.sh
@@ -4,6 +4,8 @@ CURRENT=$(pwd)
 BASENAME=$(basename "${CURRENT}")
 # Docker remove dot
 BASENAME="${BASENAME//./}"
+# Lowercase
+BASENAME="${BASENAME,,}"
 
 docker exec -u root -ti ${BASENAME}_ERPLibre_1 /bin/bash -c "\
 cd /ERPLibre; \
