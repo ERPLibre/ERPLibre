cc438c8851226ff0cbc5b3f2a3cad8b92a772e9c
Mathieu Benoit <mathben@technolibre.ca>
2023-01-07 00:06:41 -0500

    [FIX] script addons: check all return result - this will give error when execute in parallel

--- script/addons/coverage_install_addons_restore_dev.sh
+++ script/addons/coverage_install_addons_restore_dev.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 # "$1" code_generator_name
 ./script/database/db_restore.py --database "$1"
+if [[ $retVal -ne 0 ]]; then
+  echo "Error ./script/database/db_restore.py into coverage_install_addons_restore_dev.sh"
+  exit 1
+fi
+
 ./script/addons/coverage_install_addons_dev.sh "$1" "$1"
+if [[ $retVal -ne 0 ]]; then
+  echo "Error ./script/addons/coverage_install_addons_dev.sh into coverage_install_addons_restore_dev.sh"
+  exit 1
+fi
