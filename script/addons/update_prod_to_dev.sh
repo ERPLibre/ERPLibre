3f8dfcd39290eaf34ee90489a672b3c75b51b8a3
Mathieu Benoit <mathben@technolibre.ca>
2023-02-13 18:00:36 -0500

    [ADD] script update_addons_all.sh and update_prod_to_dev.sh - first script help to update all modules for a database - second script help to test a production database without affecting it

---
+++ script/addons/update_prod_to_dev.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# This script will remove mail configuration, remove backup configuration, and force admin user to test/test
+echo "Update prod to dev on BD '$1'"
+
+./script/addons/install_addons.sh "$1" user_test,disable_mail_server,disable_auto_backup
+
+retVal=$?
+if [[ $retVal -ne 0 ]]; then
+  echo "Error install_addons.sh into update_prod_to_dev.sh"
+  exit 1
+fi
