af8b7de75f627057ecc22f69ec6fc5e3a01d0a05
Alexandre Ferreira Benevides <alexandre.ferreira.benevides@gmail.com>
2023-02-25 23:52:05 -0500

    [ADD] update of install_OSX_dependencies script
    Signed-off-by: Alexandre Ferreira Benevides <alexandreferreirabenevides@MacBook-Pro-de-Alexandre.local>

--- script/install/install_OSX_dependency.sh
+++ script/install/install_OSX_dependency.sh
@@ -13,9 +13,9 @@ EL_USER=${USER}
 # Install PostgreSQL Server
 #--------------------------------------------------
 echo  "\n---- Install PostgreSQL Server ----"
-brew install postgresql
+brew install postgresql@15
 brew install postgis
-brew services start postgresql
+brew services start postgresql@15
 
 echo  "\n---- Creating the ERPLibre PostgreSQL User  ----"
 sudo su - postgres -c "createuser -s ${EL_USER}" 2> /dev/null || true
@@ -25,7 +25,7 @@ sudo su - postgres -c "CREATE EXTENSION postgis;\nCREATE EXTENSION postgis_topol
 # Install Dependencies
 #--------------------------------------------------
 echo  "\n--- Installing Python 3 + pip3 --"
-brew install git python3 wget pyenv
+brew install git python@3.7 wget parallel mariadb
 brew link git
 brew link wget
 echo  "\n---- Installing nodeJS NPM and rtlcss for LTR support ----"
@@ -34,14 +34,14 @@ sudo npm install -g rtlcss
 sudo npm install -g less
 sudo npm install -g prettier
 sudo npm install -g prettier @prettier/plugin-xml
-yes n|pyenv install 3.7.16
-pyenv local 3.7.16
+#yes n|pyenv install 3.7.16
+#pyenv local 3.7.16
 
 echo 'export PATH="/usr/local/opt/openssl@1.1/bin:$PATH"' >> ~/.zshrc
 
 echo -e "\n---- Installing poetry for reliable python package ----"
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
-
+#curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py | python3.7
+#curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
 #--------------------------------------------------
 # Install Wkhtmltopdf if needed
 #--------------------------------------------------
