9f1d674e21bca350e83a3c9a5fc6f17442a1eb68
Mathieu Benoit <mathben@technolibre.ca>
2023-07-21 16:07:56 -0400

    [UPD] image creation odoo 14

--- script/generate_config.sh
+++ script/generate_config.sh
@@ -138,7 +138,6 @@ if [[ ${EL_MINIMAL_ADDONS} = "False" ]]; then
     printf "${EL_HOME}/doc/itpp-labs_odoo-development," >> ${EL_CONFIG_FILE}
     printf "${EL_HOME}/doc/itpp-labs_odoo-port-docs," >> ${EL_CONFIG_FILE}
     printf "${EL_HOME}/doc/itpp-labs_odoo-test-docs," >> ${EL_CONFIG_FILE}
-    printf "${EL_HOME}/doc/odoo_documentation," >> ${EL_CONFIG_FILE}
     printf "${EL_HOME}/script/OCA_maintainer-tools," >> ${EL_CONFIG_FILE}
     printf "${EL_HOME}/script/OCA_odoo-module-migrator," >> ${EL_CONFIG_FILE}
 fi
