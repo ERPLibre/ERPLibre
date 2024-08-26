cc47c081dfdbe275ca584bc5dd8be4e669ab9de8
Mathieu Benoit <mathben@technolibre.ca>
2024-08-26 14:56:20 -0400

    [FIX] script remove logic to use .venv/bin/python
    - replace it by source

--- script/install/install_locally.sh
+++ script/install/install_locally.sh
@@ -98,8 +98,6 @@ if [[ ! -f ${VENV_REPO_PATH} ]]; then
     curl https://storage.googleapis.com/git-repo-downloads/repo > ${VENV_REPO_PATH}
     chmod +x ${VENV_REPO_PATH}
     sed -i 1d ${VENV_REPO_PATH}
-    PYTHON_HASHBANG="#!./.venv/bin/python"
-    sed -i "1 i ${PYTHON_HASHBANG}" ${VENV_REPO_PATH}
 fi
 
 # Make .venv active
