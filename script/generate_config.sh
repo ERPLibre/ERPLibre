49c12176942a28be5a2eb3c046dddb02e22f943f
Mathieu Benoit <mathben@technolibre.ca>
2024-10-15 22:10:02 -0400

    [FIX] config gen update fix odoo version specification
    - generate_config default workers 2
    - and dev will run config_update_dev

--- script/generate_config.sh
+++ script/generate_config.sh
@@ -146,10 +146,11 @@ printf "\n" >> "${EL_CONFIG_FILE}"
 
 printf "max_cron_threads = 2\n" >> "${EL_CONFIG_FILE}"
 
+printf "workers = 2\n" >> "${EL_CONFIG_FILE}"
 if [[ ${EL_INSTALL_NGINX} = "True" ]]; then
-    printf "workers = 2\n" >> "${EL_CONFIG_FILE}"
     printf "xmlrpc_interface = 127.0.0.1\n" >> "${EL_CONFIG_FILE}"
     printf "proxy_mode = True\n" >> "${EL_CONFIG_FILE}"
-else
-    printf "workers = 0\n" >> "${EL_CONFIG_FILE}"
 fi
+
+# Update and fix the config.conf
+make config_update
