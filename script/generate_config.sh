5c3b4af8f0d4e0c1d789873dbad365bf29bc7e8f
Mathieu Benoit <mathben@technolibre.ca>
2024-10-18 03:52:58 -0400

    wip

--- script/generate_config.sh
+++ script/generate_config.sh
@@ -24,7 +24,7 @@ printf "xmlrpc_port = ${EL_PORT}\n" >> "${EL_CONFIG_FILE}"
 printf "longpolling_port = ${EL_LONGPOLLING_PORT}\n" >> "${EL_CONFIG_FILE}"
 
 printf "addons_path = ${EL_HOME_ODOO}/addons,${EL_HOME}/addons.odoo${EL_ODOO_VERSION}/addons," >> "${EL_CONFIG_FILE}"
-printf "${EL_HOME}/addons/OCA_web," >> "${EL_CONFIG_FILE}"
+
 if [[ ${EL_MINIMAL_ADDONS} = "False" ]]; then
     printf "${EL_HOME}/addons.odoo${EL_ODOO_VERSION}/CybroOdoo_CybroAddons," >> "${EL_CONFIG_FILE}"
     printf "${EL_HOME}/addons.odoo${EL_ODOO_VERSION}/CybroOdoo_OpenHRMS," >> "${EL_CONFIG_FILE}"
