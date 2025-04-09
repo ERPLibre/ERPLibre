a36592073c3620a03067769402e830ff0bf9e210
Mathieu Benoit <mathben@technolibre.ca>
2025-04-08 23:54:22 -0400

    [ADD] script installation arch linux

--- script/install/install_dev.sh
+++ script/install/install_dev.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
 if [[ "${OSTYPE}" == "linux-gnu" ]]; then
-    OS=$(lsb_release -si)
-    VERSION=$(cat /etc/issue)
-    if [[ "${OS}" == *"Ubuntu"* ]]; then
-        if [[  "${VERSION}" == Ubuntu\ 18.04* || "${VERSION}" == Ubuntu\ 20.04* || "${VERSION}" == Ubuntu\ 22.04* || "${VERSION}" == Ubuntu\ 22.10* || "${VERSION}" == Ubuntu\ 23.04* || "${VERSION}" == Ubuntu\ 23.10* || "${VERSION}" == Ubuntu\ 24.04* ]]; then
+    source /etc/os-release
+    if [[ "${ID}" == "ubuntu" ]]; then
+        if [[  "${VERSION_ID}" == "18.04" || "${VERSION_ID}" == "20.04" || "${VERSION_ID}" == "22.04" || "${VERSION_ID}" == "22.10" || "${VERSION_ID}" == "23.04" || "${VERSION_ID}" == "23.10" || "${VERSION_ID}" == "24.04" ]]; then
             echo  "\n---- linux-gnu installation process started ----"
             ./script/install/install_debian_dependency.sh
         else
-            echo "Your version is not supported, only support 18.04, 20.04 and 22.04 - 24.04 : ${VERSION}"
+            echo "Your version is not supported, only support 18.04, 20.04 and 22.04 - 24.04 : ${VERSION_ID}"
         fi
-    elif [[ "${OS}" == *"Debian"* ]]; then
+    elif [[ "${ID}" == "debian" ]]; then
         ./script/install/install_debian_dependency.sh
+    elif [[ "${ID}" == "arch" ]]; then
+        ./script/install/install_arch_linux.sh
     else
         ./script/install/install_debian_dependency.sh
         echo "Your Linux system is not supported, only support Ubuntu 18.04 or Ubuntu 20.04 or Ubuntu 22.04 - Ubuntu 23.10 - Ubuntu 24.04 ."
