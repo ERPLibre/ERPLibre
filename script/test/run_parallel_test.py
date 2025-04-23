0f7f6abf12ab8233a02ee3ea9c29f2de04562aff
Mathieu Benoit <mathben@technolibre.ca>
2025-04-23 04:38:45 -0400

    [FIX] script test: disable test and support multiple odoo logic

--- script/test/run_parallel_test.py
+++ script/test/run_parallel_test.py
@@ -41,6 +41,11 @@ with open(".odoo-version", "r") as f:
 LOG_FILE = "./.venv/make_test.log"
 CONFIG_TESTCASE_JSON = f"./script/test/config_testcase.odoo{ODOO_VERSION}.json"
 
+with open(".odoo-version", "r") as f:
+    ODOO_VERSION = f.readline()
+
+LOG_FILE = "./.venv/make_test.log"
+CONFIG_TESTCASE_JSON = f"./script/test/config_testcase.odoo{ODOO_VERSION}.json"
 
 def get_config():
     parser = argparse.ArgumentParser(
