8f225c231bbc08f498ebd02ff34804f5737470a4
Mathieu Benoit <mathben@technolibre.ca>
2025-06-06 02:03:22 -0400

    Test implement and support big image

--- script/stream_deck/keyboard_talk.py
+++ script/stream_deck/keyboard_talk.py
@@ -1,8 +1,31 @@
+#!/usr/bin/env python3
+# © 2021-2024 TechnoLibre (http://www.technolibre.ca)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
 import keyboard
 import time
-time.sleep(4)
+import sys
+import subprocess
+try:
+   result = subprocess.run(['wmctrl', '-l'], capture_output=True, text=True, check=True)
+   lines = result.stdout.splitlines()
+   for line in lines:
+       if "Terminal" in line:
+           window_id = line.split()[0]
+           subprocess.run(['wmctrl', '-i', '-a', window_id], check=True)
+           # return
+           break
+except subprocess.CalledProcessError as e:
+   print(f"Error: {e}")
+   sys.exit(0)
+# time.sleep(1)
+# modifiers = ['ctrl', 'alt', 'shift', 'windows']
+# keyboard.send('windows+esc')
+time.sleep(1)
 keyboard.write("""
 1
 1
-1""",delay=0.5)
+1
+y
+""",delay=0.5)
 
