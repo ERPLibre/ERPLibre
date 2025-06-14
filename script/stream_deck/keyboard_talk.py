f20298b3923702692e5e715b38f3db000095b177
Mathieu Benoit <mathben@technolibre.ca>
2025-06-14 02:04:04 -0400

    [UPD] script stream_deck: detect collision, format, output debug, algo resize

--- script/stream_deck/keyboard_talk.py
+++ script/stream_deck/keyboard_talk.py
@@ -2,30 +2,36 @@
 # © 2021-2024 TechnoLibre (http://www.technolibre.ca)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-import keyboard
-import time
-import sys
 import subprocess
+import sys
+import time
+
+import keyboard
+
 try:
-   result = subprocess.run(['wmctrl', '-l'], capture_output=True, text=True, check=True)
-   lines = result.stdout.splitlines()
-   for line in lines:
-       if "Terminal" in line:
-           window_id = line.split()[0]
-           subprocess.run(['wmctrl', '-i', '-a', window_id], check=True)
-           # return
-           break
+    result = subprocess.run(
+        ["wmctrl", "-l"], capture_output=True, text=True, check=True
+    )
+    lines = result.stdout.splitlines()
+    for line in lines:
+        if "Terminal" in line:
+            window_id = line.split()[0]
+            subprocess.run(["wmctrl", "-i", "-a", window_id], check=True)
+            # return
+            break
 except subprocess.CalledProcessError as e:
-   print(f"Error: {e}")
-   sys.exit(0)
+    print(f"Error: {e}")
+    sys.exit(0)
 # time.sleep(1)
 # modifiers = ['ctrl', 'alt', 'shift', 'windows']
 # keyboard.send('windows+esc')
 time.sleep(1)
-keyboard.write("""
+keyboard.write(
+    """
 1
 1
 1
 y
-""",delay=0.5)
-
+""",
+    delay=0.5,
+)
