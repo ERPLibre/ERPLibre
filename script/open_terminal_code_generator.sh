90748433c969f43b9f25fd6ef658d8f81704b4d7
Mathieu Benoit <mathben@technolibre.ca>
2025-07-30 23:24:00 -0400

    [FIX] open terminal script without gnome
    - not working

--- script/open_terminal_code_generator.sh
+++ script/open_terminal_code_generator.sh
@@ -2,58 +2,14 @@
 # Open a new gnome-terminal with different path on new tab
 ODOO_VERSION=$(cat .odoo-version)
 working_path=$(readlink -f .)
-paths=(
-  "${working_path}/"
-  "${working_path}/"
-  "${working_path}/addons.odoo${ODOO_VERSION}/ERPLibre_erplibre_addons"
-  "${working_path}/addons.odoo${ODOO_VERSION}/TechnoLibre_odoo-code-generator"
-  "${working_path}/addons.odoo${ODOO_VERSION}/TechnoLibre_odoo-code-generator-template"
+paths="${working_path}/
+${working_path}/
+${working_path}/addons.odoo${ODOO_VERSION}/ERPLibre_erplibre_addons
+${working_path}/addons.odoo${ODOO_VERSION}/TechnoLibre_odoo-code-generator
+${working_path}/addons.odoo${ODOO_VERSION}/TechnoLibre_odoo-code-generator-template"
+
 #  "${working_path}/addons.odoo${ODOO_VERSION}/OCA_server-tools"
 
-)
-
-first_iteration=true
-second_iteration=true
-if [[ "${OSTYPE}" == "linux-gnu" ]]; then
-  cmd_before="cd "
-  cmd_after_first=";gnome-terminal --tab -- bash -c 'source ./.venv.erplibre/bin/activate;git status;bash';"
-  cmd_after=";gnome-terminal --tab -- bash -c 'git status;bash';"
-  LONGCMD=""
-  for t in "${paths[@]}"; do
-    if [[ ! -e "${t}" ]]; then
-      continue
-    fi
-    if $first_iteration; then
-      LONGCMD+="${cmd_before}${t}${cmd_after_first}"
-      first_iteration=false
-    elif $second_iteration; then
-      LONGCMD+="${cmd_before}${t}${cmd_after_first}"
-      second_iteration=false
-    else
-      LONGCMD+="${cmd_before}${t}${cmd_after}"
-    fi
-  done
-  gnome-terminal --window -- bash -c "${LONGCMD}"
-elif [[ "${OSTYPE}" == "darwin"* ]]; then
-  paths=("${paths[@]:1}")
-
-  # Initialisation de la commande osascript
-  osascript_command="osascript -e 'tell application \"Terminal\"'"
-
-  # Boucle pour ajouter des commandes pour ouvrir de nouveaux onglets et exécuter les scripts batch
-  for t in "${paths[@]}"; do
-    if $first_iteration; then
-      osascript_command+=" -e 'tell application \"System Events\" to keystroke \"t\" using {command down}' -e 'delay 0.1' -e 'do script \"cd ${t}; source ./.venv.erplibre/bin/activate; git status\" in front window'"
-      first_iteration=false
-    elif $second_iteration; then
-      osascript_command+=" -e 'tell application \"System Events\" to keystroke \"t\" using {command down}' -e 'delay 0.1' -e 'do script \"cd ${t}; source ./.venv.erplibre/bin/activate; git status\" in front window'"
-      second_iteration=false
-    else
-      osascript_command+=" -e 'tell application \"System Events\" to keystroke \"t\" using {command down}' -e 'delay 0.1' -e 'do script \"cd ${t}; git status\" in front window'"
-    fi
-  done
-  osascript_command+=" -e 'end tell'"
-
-  # Exécution de la commande osascript
-  eval "$osascript_command"
-fi
+cmd="git status"
+#echo "${paths}"
+./script/terminal/open_terminal.sh "$cmd" "$paths"
---
+++ script/terminal/open_terminal.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/env bash
+
+CMD_TO_EXEC="$1"
+PATHS_RAW="$2"
+#paths="$2"
+echo "Script start open_terminal.sh"
+echo "$PATHS_RAW"
+IFS=$'\n' read -r -d '' -a paths <<< "$PATHS_RAW"
+echo "$path"
+SOURCE_CMD="source ./.venv.erplibre/bin/activate"
+FIRST_ITERATION=true
+
+which gnome-terminal
+GNOME_TERMINAL_CMD=$(which gnome-terminal)
+
+HAS_GNOME_TERMINAL=false
+HAS_TELL_TERMINAL=false
+if command -v gnome-terminal &>/dev/null; then
+  HAS_GNOME_TERMINAL=true
+  echo "Detect gnome-terminal"
+#elif command -v tell &>/dev/null; then
+elif [[ "${OSTYPE}" == "darwin"* ]] && command -v osascript &>/dev/null; then
+  echo "Detect osascript Darwin"
+  HAS_TELL_TERMINAL=true
+else
+  echo "Detect cli"
+fi
+
+if [[ -z "$paths" ]]; then
+  working_path=$(readlink -f .)
+  paths="${working_path}/"
+fi
+
+#if [[ "${OSTYPE}" == "linux-gnu" ]]; then
+if [ "$HAS_GNOME_TERMINAL" = true ]; then
+  CMD_BEFORE="cd "
+  CMD_AFTER_FIRST=";gnome-terminal --tab -- /bin/bash -c '${SOURCE_CMD};${CMD_TO_EXEC};bash';"
+  CMD_AFTER=";gnome-terminal --tab -- /bin/bash -c '${CMD_TO_EXEC};bash';"
+  LONGCMD=""
+  for PATH in "${paths[@]}"; do
+    if [[ ! -e "${PATH}" ]]; then
+      continue
+    fi
+    if $FIRST_ITERATION; then
+      LONGCMD+="${CMD_BEFORE}${PATH}${CMD_AFTER_FIRST}"
+      FIRST_ITERATION=false
+    else
+      LONGCMD+="${CMD_BEFORE}${PATH}${CMD_AFTER}"
+    fi
+  done
+  echo "${LONGCMD}"
+  echo "${GNOME_TERMINAL_CMD}"
+#  $GNOME_TERMINAL_CMD --window -- /bin/bash -c "ls"
+  $GNOME_TERMINAL_CMD --window -- /bin/bash -c "${LONGCMD}"
+elif [ "$HAS_TELL_TERMINAL" = true ]; then
+  # Initialisation de la commande osascript
+  osascript_command="osascript -e 'tell application \"Terminal\"'"
+
+  # Boucle pour ajouter des commandes pour ouvrir de nouveaux onglets et exécuter les scripts batch
+  for PATH in "${paths[@]}"; do
+    if [[ ! -e "${PATH}" ]]; then
+      continue
+    fi
+    if $FIRST_ITERATION; then
+      osascript_command+=" -e 'tell application \"System Events\" to keystroke \"PATH\" using {command down}' -e 'delay 0.1' -e 'do script \"cd ${PATH}; ${SOURCE_CMD}; ${CMD_TO_EXEC}\" in front window'"
+      FIRST_ITERATION=false
+    else
+      osascript_command+=" -e 'tell application \"System Events\" to keystroke \"PATH\" using {command down}' -e 'delay 0.1' -e 'do script \"cd ${PATH}; ${CMD_TO_EXEC}\" in front window'"
+    fi
+  done
+  osascript_command+=" -e 'end tell'"
+
+  # Exécution de la commande osascript
+  eval "$osascript_command"
+else
+  echo "CLI"
+  for PATH in "${paths[@]}"; do
+    if [[ ! -e "${PATH}" ]]; then
+      continue
+    fi
+    cd ${PATH}; ${SOURCE_CMD}; ${CMD_TO_EXEC}
+  done
+fi
