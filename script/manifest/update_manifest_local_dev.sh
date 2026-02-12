268cbae7120c751fd52af1e799e26e6aa2eeeb94
Mathieu Benoit <mathben@technolibre.ca>
2026-02-12 00:52:32 -0500

    [FIX] script with nproc jobs, support osx

--- script/manifest/update_manifest_local_dev.sh
+++ script/manifest/update_manifest_local_dev.sh
@@ -14,10 +14,16 @@ else
   MANIFEST_TARGET="$EL_MANIFEST_DEV"
 fi
 
+if command -v nproc >/dev/null 2>&1; then
+  JOBS="$(nproc --all)"
+else
+  JOBS="$(sysctl -n hw.ncpu)"
+fi
+
 # Generate local manifest
 .venv.erplibre/bin/python ./script/git/git_merge_repo_manifest.py --output .repo/local_manifests/erplibre_manifest.xml --with_OCA
 
 .venv.erplibre/bin/repo init -u git://127.0.0.1:9418/ -b $(git rev-parse --verify HEAD) -m ${MANIFEST_TARGET} "$@"
-.venv.erplibre/bin/repo sync -c -j $(nproc --all) -v -m ${MANIFEST_TARGET}
+.venv.erplibre/bin/repo sync -c -j "$JOBS" -v -m ${MANIFEST_TARGET}
 
 kill ${DAEMON_PID}
--- script/manifest/update_manifest_prod.sh
+++ script/manifest/update_manifest_prod.sh
@@ -5,6 +5,12 @@
 #EL_MANIFEST_PROD="./default.xml"
 #EL_MANIFEST_DEV="./manifest/default.dev.xml"
 
+if command -v nproc >/dev/null 2>&1; then
+  JOBS="$(nproc --all)"
+else
+  JOBS="$(sysctl -n hw.ncpu)"
+fi
+
 # Update git-repo
 .venv.erplibre/bin/repo init -u https://github.com/ERPLibre/ERPLibre -b $(git rev-parse --verify HEAD)
-.venv.erplibre/bin/repo sync -v -j $(nproc --all)
+.venv.erplibre/bin/repo sync -v -j "$JOBS"
