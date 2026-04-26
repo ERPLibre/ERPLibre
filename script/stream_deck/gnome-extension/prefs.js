3ea9b02d07589a9168e6b9861bd0ee93ad8cbc50
Mathieu Benoit <mathben@technolibre.ca>
2026-04-26 04:11:35 -0400

    [ADD] stream_deck/gnome-extension: prefs skeleton with Buttons page

---
+++ script/stream_deck/gnome-extension/prefs.js
@@ -0,0 +1,41 @@
+import Adw from 'gi://Adw';
+import Gio from 'gi://Gio';
+import {ExtensionPreferences}
+    from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
+
+const INDICATORS = [
+    {id: 'controller', label: 'Controller'},
+    {id: 'pencil',     label: 'Pencil'},
+    {id: 'film',       label: 'Film'},
+    {id: 'erplibre',   label: 'ERPLibre'},
+    {id: 'network',    label: 'Network'},
+    {id: 'device',     label: 'Device'},
+];
+
+export default class StreamDeckTilerPrefs extends ExtensionPreferences {
+    fillPreferencesWindow(window) {
+        const settings = this.getSettings();
+
+        const buttonsPage = new Adw.PreferencesPage({
+            title: 'Buttons',
+            icon_name: 'view-grid-symbolic',
+        });
+
+        const togglesGroup = new Adw.PreferencesGroup({
+            title: 'Indicators',
+            description: 'Toggle each panel button on or off.',
+        });
+        for (const ind of INDICATORS) {
+            const row = new Adw.SwitchRow({
+                title: ind.label,
+                subtitle: `enable-${ind.id}`,
+            });
+            settings.bind(`enable-${ind.id}`, row, 'active',
+                Gio.SettingsBindFlags.DEFAULT);
+            togglesGroup.add(row);
+        }
+        buttonsPage.add(togglesGroup);
+
+        window.add(buttonsPage);
+    }
+}
